acdc73e7f3255cf182f46e22609e7ea051e6ec83
Danvargas23 <sm90565671@idat.edu.pe>
2023-07-05 08:05:05 -0500

    arreglo

---
+++ src/main/java/pe/edu/idat/restservice/CursoRepository.java
@@ -0,0 +1,7 @@
+package pe.edu.idat.restservice;
+
+import org.springframework.data.repository.CrudRepository;
+
+public interface CursoRepository extends CrudRepository<Curso,Integer> {
+    
+}
---
+++ src/main/java/pe/edu/idat/restservice/EC2RestServiceApplication.java
@@ -0,0 +1,12 @@
+package pe.edu.idat.restservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EC2RestServiceApplication {
+    
+    public static void main(String[] args) {
+        SpringApplication.run(EC2RestServiceApplication.class,args);
+    }
+}
